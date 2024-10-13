507f773ed1e41f094dfd732b5f97d9fe12d829d8
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2024-10-13 17:31:21 +0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,7 @@
+import FormikYup from './src/example/FormikYup';
+
+const App = () => {
+  return <FormikYup />;
+};
+
+export default App;
---
+++ src/example/FormikYup.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const FormikYup = () => {
+  return (
+    <View>
+      <Text>FormikYup</Text>
+    </View>
+  );
+};
+
+export default FormikYup;
+
+const styles = StyleSheet.create({});
